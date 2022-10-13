3c09d3f8ef62b5ff8da9a06249b81c712c60bf2b
hayoun <fishingest@kookmin.ac.kr>
2022-10-13 23:34:32 +0900

    [FIX] 파일 경로 수정

---
+++ week1/TodoList/Todo.js
@@ -0,0 +1,92 @@
+
+
+
+function useViewState({
+ 
+}){
+    const today = document.querySelector(".today_button"); 
+    const tomorrow = document.querySelector(".tomorrow_button");
+    const allday=document.querySelector(".allday_button")
+    const leftPanel=document.querySelector(".left-section");
+    const rightPanel=document.querySelector(".right-section");
+  
+    function click(e){
+        alert('ss');
+    }
+
+const viewToday=()=>{
+    rightPanel.classList.add("hidden");
+    leftPanel.classList.remove("hidden");
+}
+const viewTomorrow=()=>{
+    leftPanel.classList.add("hidden");
+    rightPanel.classList.remove("hidden");
+}
+const viewAllday=()=>{
+    leftPanel.classList.remove("hidden");
+    rightPanel.classList.remove("hidden");
+}
+
+
+today.addEventListener("click", viewToday)
+tomorrow.addEventListener("click",viewTomorrow)
+allday.addEventListener("click",viewAllday)
+
+
+
+}
+
+
+// function useTodoList({container,input,submit}){
+//     const onAdd=()=>{
+            
+// const currentValue=input.value;
+// const currentLi=document.createElement("li");
+// currentLi.classList.add("todo__item");
+// currentLi.innerHTML=currentValue;
+
+
+// const currentDeleteButton=document.createElement("button")
+// currentDeleteButton.innerHTML=`<span class="material-symbols-outlined">
+// delete</span>`
+// currentLi.appendChild(currentDeleteButton);
+// container.appendChild(currentLi);
+//     }
+//     submit.addEventListner("click",onAdd);
+//     input.addEventListner("keydown",e=>{
+//         if(e.keyCode==="Enter"){
+//             onAdd;
+//         }
+//     })
+// currentDeleteButton.addEventListner("click",)
+
+// }
+
+   
+    
+// //     )
+function main(){
+    
+    
+    useViewState({
+        
+        // leftPanel:$(".left-section"),
+        // rightPanel:`$(".right-section")`,
+        // todayButton:`$("#today_button")`
+    })
+    // console.log(leftPanel.classList[0])
+
+}
+
+
+
+
+main();
+
+// const todayButton=document.querySelector('.today_button nav_button')
+// console.log(todayButton)
+// todayButton.addEventListner("click",function(){
+//     alert("ss");
+// });
+
+
