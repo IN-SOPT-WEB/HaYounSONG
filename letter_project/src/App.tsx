242935d3e59d733665f595387c54b590536dc6b3
hayoun <fishingest@kookmin.ac.kr>
2022-12-24 22:49:54 +0900

    [FEAT] GlobalStyle reset 세팅

--- letter_project/src/App.tsx
+++ letter_project/src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/MainPage/Header';
 import { Router,Routes,Route, BrowserRouter } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import WritePage from './pages/WritePage';
+import GlobalStyles from './style/GlobalStyle';
 function App() {
 
   let number:string='asdf';
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <GlobalStyles/>
       <Routes>
         <Route path="/main" element={<MainPage/>}/>
         <Route path="/write" element={<WritePage/>}/>
