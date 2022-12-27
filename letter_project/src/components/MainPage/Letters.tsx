5e12df0dc1173f6332770f476ecda2b435433fba
hayoun <fishingest@kookmin.ac.kr>
2022-12-28 03:45:39 +0900

    [FEAT] Fake API 통신

--- letter_project/src/components/MainPage/Letters.tsx
+++ letter_project/src/components/MainPage/Letters.tsx
@@ -1,11 +1,32 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import styled from "styled-components"
+import axios from "axios"
+import ReactElement from 'react';
 export default function Letters(){
+    // interface LetterType {
+    //     [
+    //     writer: string,
+    //     content:string,
+    // ]
+    //   }
+    const [letterList,setletterList]=useState<any>([]);
+    const getLetter=async():Promise<void>=>{
+        const response:any=await axios.get("https://jsonplaceholder.typicode.com/comments");
+        setletterList(response.data);
+
+    }
+    useEffect(()=>{
+        getLetter();
+        console.log(letterList)
+    },[])
 return(
     <Styled.Letters>
-        <Styled.Letter></Styled.Letter>
+         {letterList===undefined ? <div>로딩중입니다..</div> : <>
+       {letterList.slice(0,3).map((index:any)=>(<div>{index.name}</div>))}</>}
+
+        
     </Styled.Letters>
-)
+)   
 }
 
 const Styled={
